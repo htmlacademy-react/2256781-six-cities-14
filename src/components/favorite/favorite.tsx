41c3ca58f2905dbea0e62cfc2c314d964be442a4
Dimaggio <zodiak-d@yandex.ru>
2023-11-27 20:24:42 +0300

    вообще убрал мемоизацию ф-ии getOffersByCity. useMemo сомневаюсь что сработает - потому, что в нем нельзя установить зависимость через параметры анонимного колбека, a memoize из loadash наставник не одобрил... так что будет без мемоизации

--- src/components/favorite/favorite.tsx
+++ src/components/favorite/favorite.tsx
@@ -3,18 +3,11 @@ import { AppRoute, TYPE_CARD } from '../../const';
 import { getOffersByCity } from '../../utils';
 import { useAppSelector } from '../../hooks';
 import { selectCitiesFromFavoritesMemo, selectFavorites } from '../../store';
-import { TCityName, TOffersPreview } from '../../types';
 import { Card } from '..';
-import { useMemo } from 'react';
 
 function Favorite(): JSX.Element {
   const cities = useAppSelector(selectCitiesFromFavoritesMemo);
   const favorites = useAppSelector(selectFavorites);
-  const getFavoritesByCityMemo = useMemo(
-    () => (offers: TOffersPreview, city: TCityName) =>
-      getOffersByCity(offers, city),
-    []
-  );
 
   return (
     <main className="page__main page__main--favorites">
@@ -32,7 +25,7 @@ function Favorite(): JSX.Element {
                   </div>
                 </div>
                 <div className="favorites__places">
-                  {getFavoritesByCityMemo(favorites, city).map((offer) => (
+                  {getOffersByCity(favorites, city).map((offer) => (
                     <Card
                       key={offer.id}
                       offer={offer}
